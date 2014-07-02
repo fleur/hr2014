e85f74dd844dbdd86640a3f1507439b44dceca50
Fleur Dragan <fleur@obscure.org>
2014-07-02 11:53:31 -0700

    initial recursion solution

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var rv = "";
+        
+    if (obj == undefined) {
+
+        rv += 'null';
+        
+    } else if (typeof(obj) == 'string') {
+        
+        rv += '"' + obj + '"';
+        
+    } else if ((typeof(obj) == 'number') ||
+               (typeof(obj) == 'boolean')) {
+        
+        rv += obj;            
+    
+    } else if (Array.isArray(obj)) {
+        
+        rv += '[';
+        rv += obj.map(stringifyJSON).join(',');
+        rv += ']';
+
+    } else {
+        var hasValues = false;
+        
+        rv += '{';
+        for (var k in obj) {
+            console.log("key: "+JSON.stringify(k));
+            if ((k != 'functions') && (k != "undefined")) {
+                hasValues = true;
+                rv += stringifyJSON(k) + ':';
+                rv += stringifyJSON(obj[k]);
+                rv += ",";
+            }
+        }
+        
+        if (hasValues) {
+            rv = rv.slice(0, -1);
+        }
+        
+        rv += '}';
+    }
+        
+    return rv;    
 };
