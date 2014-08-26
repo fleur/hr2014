409955997c5b0eca2a4f4331d1dd73ded0852ef8
fleur <fleur@obscure.org>
2014-08-26 15:52:32 -0700

    added shelling out from server to run phantom scrape

--- phantom.js
+++ phantom.js
@@ -3,8 +3,8 @@ var webpage = require('webpage');
 var page = webpage.create();
 var url = 'http://www.magicformulainvesting.com/Account/LogOn';
 /* waitFor from http://newspaint.wordpress.com/2013/04/05/waiting-for-page-to-load-in-phantomjs/ */
-function waitFor( page, selector, expiry, callback ) {
-  console.log( "- waitFor( " + selector + ", " + expiry + " )");
+var waitFor = function( page, selector, expiry, callback ) {
+  //console.log( "- waitFor( " + selector + ", " + expiry + " )");
 
   // try and fetch the desired element from the page
   var result = page.evaluate(function (selector) {
@@ -13,7 +13,7 @@ function waitFor( page, selector, expiry, callback ) {
 
   // if desired element found then call callback after 50ms
   if ( result ) {
-    console.log( "- trigger " + selector + " found" );
+    //console.log( "- trigger " + selector + " found" );
     window.setTimeout(function () {
           callback( true );
     }, 50);
@@ -23,7 +23,7 @@ function waitFor( page, selector, expiry, callback ) {
   // determine whether timeout is triggered
   var finish = (new Date()).getTime();
   if ( finish > expiry ) {
-    console.log( "- timed out" );
+    //console.log( "- timed out" );
     callback( false );
     return;
   }
@@ -42,25 +42,22 @@ page.onError = function (msg, trace) {
   });
 };
 
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
-};
+// page.onConsoleMessage = function(msg) {
+//   console.log(msg);
+// };
 
-page.onNavigationRequested = function(url, type, willNavigate, main) {
-  console.log('Trying to navigate to: ' + url);
-  console.log('Caused by: ' + type);
-  console.log('Will actually navigate: ' + willNavigate);
-  console.log('Sent from the page\'s main frame: ' + main);
-};
+// page.onNavigationRequested = function(url, type, willNavigate, main) {
+//   console.log('Trying to navigate to: ' + url);
+//   console.log('Caused by: ' + type);
+//   console.log('Will actually navigate: ' + willNavigate);
+//   console.log('Sent from the page\'s main frame: ' + main);
+// };
 
 var count = 0;
 page.onLoadFinished = function() {
 
-  console.log("step: ", count);
-  page.render("phantom"+count+".png");
   if (count === 0) {
     // log in
-    console.log("in page.evaluate");
     page.evaluate(function() {
       jQuery('#Email').val('hrsolo16@gmail.com');
       jQuery('#Password').val('w1nst0n');
@@ -69,27 +66,22 @@ page.onLoadFinished = function() {
     count++;
 
   } else if (count === 1) {
-    console.log("should have logged in");
+
     page.evaluate(function() {
       jQuery('#MinimumMarketCap').val(142);
       jQuery('#stocks').click();
     });
 
-  // now we need to wait for the stocks to show up
-  waitFor(page, "#report", (new Date()).getTime() + 100000, function(status) {
-    if (!status) {
-      console.log("timed out waiting for report to load: ", status);
-    }
-    page.render("pjsfinal.png");
-    phantom.exit();
-});
-
-  } else if (count === 2) {
-    console.log("should have gotten stocks at this point");
+    // now we need to wait for the stocks to show up
+    waitFor(page, "#report", (new Date()).getTime() + 100000, function(status) {
+      if (!status) {
+        console.log("timed out waiting for report to load: ", status);
+      }
+      page.render("pjsfinal.png");
+      console.log(page.content);
+      phantom.exit();
+    });
   }
-
-  console.log("url: ", document.URL);
-
 };
 
 page.open(url, function(status) {
@@ -97,12 +89,5 @@ page.open(url, function(status) {
     console.log("failed to open page: ", url);
     phantom.exit();
   }
-  console.log("opened page");
-
-
-
-
-  // at this point we should have a page with stocks on it
-
-
+  //console.log("opened page");
 });
--- server.js
+++ server.js
@@ -1,16 +1,62 @@
-var static = require('node-static');
+// var static = require('node-static');
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new static.Server('./');
+// //
+// // Create a node-static server instance to serve the './public' folder
+// //
+// var file = new static.Server('./');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(8080);
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//         //
+//         // Serve files!
+//         //
+//         file.serve(request, response);
+//     }).resume();
+// }).listen(8080);
 
+//var util = require('./server_util.js');
+var path = require('path')
+var express = require('express');
+var session = require('express-session');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs');
+var app = express();
+
+
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
+// Parse forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  //res.end();
+});
+
+app.get('/api/ticker', function(req, res) {
+  var binPath = phantomjs.path
+  var childArgs = [
+    path.join(__dirname, 'phantom.js'),
+  ];
+
+  childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+
+    console.log(stdout);
+    res.send(stdout);
+    res.end();
+  });
+
+
+});
+
+app.get('/*', function(req, res) {
+  res.send(404);
+  res.end();
+});
+
+
+console.log('listening on 8080');
+app.listen(8080);
